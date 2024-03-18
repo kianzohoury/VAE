6e0683df4adc73b9364fbe98859b114703d38bec
kianzohoury <kzohoury@berkeley.edu>
2024-03-18 04:52:58 -0700

    working on readme

--- plot.py
+++ plot.py
@@ -168,6 +168,72 @@ def plot_reconstruction_grid(
     fig.savefig(f"{model_dir}/plots/reconstruction_grid.jpg")
 
 
+def plot_reconstructed_digits(
+    model_dir: str,
+    dataset: str = "mnist"
+):
+    """Plots a grid comparing images with generated reconstructions."""
+    datasets = utils.load_dataset(
+        name=dataset,
+        splits=("test"),
+        val_size=0
+    )
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    for checkpoint in list(Path(model_dir).rglob("*.pth")):
+        state_dict = torch.load(checkpoint, map_location=device)
+        model_type = state_dict["config"].pop("model_type")
+
+        # initialize model
+        model = utils.init_model(model_type, **state_dict["config"]).to(
+            device)
+        num_latent = state_dict["config"]["num_latent"]
+        num_classes = state_dict["config"]["num_classes"]
+        model.load_state_dict(state_dict["model"])
+        model.eval()
+
+        fig, ax = plt.subplots(
+            nrows=2,
+            ncols=10,
+            constrained_layout=True,
+            figsize=(2, 10)
+        )
+        for class_idx in range(10):
+            indices = []
+            for idx, sample in enumerate(datasets["test"]):
+                if sample[-1] == class_idx:
+                    indices.append(idx)
+
+            # create dataloader
+            test_loader = DataLoader(
+                dataset=torch.utils.Subset(datasets["test"], indices),
+                batch_size=1,
+                shuffle=True,
+                pin_memory=True
+            )
+
+
+            img, label = next(iter(test_loader))
+            ax[0][class_idx].imshow(img[0].squeeze(0), cmap="gray")
+            ax[0][class_idx].axis("off")
+            ax[0][class_idx].set_title(f"Original ({class_idx})")
+
+            if model_type == "ConditionalVAE":
+                y = nn.functional.one_hot(label, num_classes)
+                gen_img = model(img.to(device), y.to(device))
+            else:
+                gen_img = model(img.to(device))
+            if isinstance(gen_img, tuple):
+                gen_img = gen_img[0]
+            gen_img = gen_img.detach().cpu()
+            ax[1][class_idx].imshow(gen_img[0].squeeze(0), cmap="gray")
+            ax[1][class_idx].axis("off")
+            ax[1][class_idx].set_title("Reconstruction")
+
+    fig.suptitle(f"{'Digits' if dataset == 'mnist' else 'Class'}")
+    fig.savefig(f"{model_dir}/plots/{model_type}_reconstruction_grid.jpg")
+
+
 def plot_vae_decoding_grid(
     model_dir: str,
     img_dim: Tuple[int, ...],
