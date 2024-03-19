97a16eea21f47ef8d7419d8cccd217955e56aa2f
kianzohoury <kzohoury@berkeley.edu>
2024-03-18 22:27:29 -0700

    debugging plot

--- plot.py
+++ plot.py
@@ -220,7 +220,7 @@ def plot_comparison(
                 img.moveaxis(0, -1)[0], cmap="gray"
             )
         else:
-            ax[0][class_idx].imshow(img.moveaxis(0, -1)[0])
+            ax[0][class_idx].imshow(img.moveaxis(0, -1)[0], cmap=None)
 
         ax[0][class_idx].set_xticks([])
         ax[0][class_idx].set_yticks([])
@@ -235,10 +235,11 @@ def plot_comparison(
 
         gen_img = gen_img.detach().cpu()
         gen_img = gen_img.moveaxis(0, -1)[0]
+
         if dataset == "mnist":
             ax[1][class_idx].imshow(gen_img, cmap="gray")
         else:
-            ax[1][class_idx].imshow(gen_img)
+            ax[1][class_idx].imshow(gen_img, cmap=None)
         ax[1][class_idx].set_xticks([])
         ax[1][class_idx].set_yticks([])
 
